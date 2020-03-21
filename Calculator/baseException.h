5121ef9ba2678b752f6244a5f5614f2f4393719f
vincentlie18 <vincentiuslienardo@gmail.com>
2020-03-21 16:26:29 +0700

    Updated baseException.h

--- Calculator/baseException.h
+++ Calculator/baseException.h
@@ -1,10 +1,23 @@
 #ifndef BASEEXCEPTION_H
 #define BASEEXCEPTION_H
+
 template <class T>
 class BaseException {
-public:
-    // menuliskan pesan kesalahan ke stdout
-    virtual void printMessage() = 0;
+    public:
+        // menuliskan pesan kesalahan ke stdout
+        virtual void printMessage() = 0;
+};
+
+class InvalidNumberException : public BaseException {
+    private:
+        T number;
+    public:
+        InvalidNumberException(T number) {
+            this->number = number;
+        }
+        void printMessage() {
+            cout << "Bilangan " << number << " tidak valid" << endl;
+        }
 };
 
 #endif // BASEEXCEPTION_H
